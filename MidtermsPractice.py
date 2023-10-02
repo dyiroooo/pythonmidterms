ea2aee563bb08e6263c1a5dcdbe9c5f03b225882
dyiroooo <20200097m.ibia.jiroallen.bscs@gmail.com>
2023-10-02 17:53:59 +0800

    [Problem 5] Fixed Friendly Pair Numbers Bug

--- MidtermsPractice.py
+++ MidtermsPractice.py
@@ -34,7 +34,7 @@ def sumnumber(number):
     return totalsum
 
 
-def factors(number):
+def factorsSolo(number):
     factors = []
     for i in range(1, number):
         if number % i == 0:
@@ -42,8 +42,16 @@ def factors(number):
     return factors
 
 
+def factorsInclude(number):
+    factors = []
+    for i in range(1, number + 1):
+        if number % i == 0:
+            factors.append(i)
+    return factors
+
+
 def isperfectnumber(number):
-    numList = factors(number)
+    numList = factorsSolo(number)
     perfect = sum(numList)
     if perfect != number:
         return "is not a perfect number."
@@ -52,13 +60,16 @@ def isperfectnumber(number):
 
 
 def isfriendlypair(number1, number2):
-    # Get their factors first
-    numfac1 = factors(number1)
-    numfac2 = factors(number2)
+    # Get their factors first # Let number1 = n and number2 = m
+    numfac1 = factorsInclude(number1)
+    numfac2 = factorsInclude(number2)
     # Check their sum
     sumfac1 = sum(numfac1)
     sumfac2 = sum(numfac2)
-    if sumfac1 == number2 and sumfac2 == number1:
+    # Divide them with n and m
+    checker1 = sumfac1 / number1
+    checker2 = sumfac2 / number2
+    if checker1 == checker2:
         return f"{number1} and {number2} are friendly pairs."
     else:
         return f"{number1} and {number2} are not friendly pairs."
